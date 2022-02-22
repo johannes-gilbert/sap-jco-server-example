b407d729136a6ee311e5b3bed9c6fa8e3344a661
Johannes Gilbert <johannes.gilbert@sap.com>
2022-02-22 07:59:16 +0100

    switch from javax.ws.rs.client.Client to java.net.http.HttpClient

--- src/com/sap/HttpCaller.java
+++ src/com/sap/HttpCaller.java
@@ -1,20 +1,18 @@
 package com.sap;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 
@@ -24,7 +22,7 @@ public class HttpCaller {
 	
 	private SSLContext context;
 	
-	private Client client;
+	private HttpClient client;
 	
 	public HttpCaller() {
 	}
@@ -56,33 +54,22 @@ public class HttpCaller {
 	public void initializeClient() {	
 		// In case you are not using any proxy delete these lines.
         // In case you are using a proxy replace <host> and <port>.
-		System.setProperty ("https.proxyHost", "<host>");
+		/*System.setProperty ("https.proxyHost", "<host>");
 		System.setProperty ("https.proxyPort", "<port>");
 		System.setProperty ("http.proxyHost", "<host>");
-		System.setProperty ("http.proxyPort", "<port>");
+		System.setProperty ("http.proxyPort", "<port>");*/
 		    
 		// Build a client.
-		client = ClientBuilder.newBuilder()
-				.sslContext(context)
-				.hostnameVerifier(new HostnameVerifier() {
-					@Override
-					public boolean verify(String hostname, SSLSession session) {
-						// Just accept any host. Do not do an actual verification.
-						return true;
-					}
-				})
-				.build();
+		client = HttpClient.newBuilder().sslContext(context).build();
 	}
 	
-	public String invokeGet(String uri) {
+	public String invokeGet(String uri) throws IOException, InterruptedException {
 		logger.info("URL: "+uri);
-		WebTarget target = client.target(uri);
-		Invocation.Builder invocationBuilder = target.request();
-		// Set necessary headers.
-		invocationBuilder.header("Accept","*/*");
-		invocationBuilder.header("Accept-Encoding","gzip, deflate, sdch, br");
-		invocationBuilder.header("Accept-Language","de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4");
-		Response response = invocationBuilder.get();
-		return response.readEntity(String.class);
+		HttpRequest request = HttpRequest.newBuilder(URI.create(uri))
+				.header("Accept","*/*")
+				.header("Accept-Language","de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4")
+				.build();
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		return response.body();
 	}
 }
